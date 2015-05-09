f2f9abf39a6b9030bac4e39646f80f533e826ae0
danielmontesano <daniel.montesano@gmail.com>
2015-05-09 12:46:16 +0200

    Despliegue de DB en heroku

--- models/models.js
+++ models/models.js
@@ -2,33 +2,33 @@ var path = require('path');
 
 //Postgres DATABASE_URL = postgress://user:passwd@host:port/database
 //SQlite   DATABASE_URL = sqlite://:@:/
-// var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-// var DB_name 	= (url[6]||null);
-// var user 		= (url[2]||null);
-// var pwd 		= (url[3]||null);
-// var protocol 	= (url[1]||null);
-// var dialect 	= (url[1]||null);
-// var port 		= (url[5]||null);
-// var host 		= (url[4]||null);
-// var storage 	= process.env.DATABASE_STORAGE;
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name 	= (url[6]||null);
+var user 		= (url[2]||null);
+var pwd 		= (url[3]||null);
+var protocol 	= (url[1]||null);
+var dialect 	= (url[1]||null);
+var port 		= (url[5]||null);
+var host 		= (url[4]||null);
+var storage 	= process.env.DATABASE_STORAGE;
 
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 //User BBDD SQlite o Postgress
-// var sequelize = new Sequelize(DB_name, user, pwd,
-// 	{	dialect: dialect, 
-// 		protocol: protocol,
-// 		storage: storage,
-// 		port: port,
-// 		host: host,
-// 		omitNULL: true 
-// 	}
-// );
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{	dialect: dialect, 
+		protocol: protocol,
+		port: port,
+		host: host,
+		storage: storage,
+		omitNULL: true 
+	}
+);
 
-var sequelize = new Sequelize(null, null, null,
-				{dialect: "sqlite", storage: "quiz.sqlite"}
-				);
+// var sequelize = new Sequelize(null, null, null,
+// 				{dialect: "sqlite", storage: "quiz.sqlite"}
+// 				);
 
 //Importar la definicion de la tabla quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
